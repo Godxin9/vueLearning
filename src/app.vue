1f6c16fa138765c6ba0c2877e5c1ca11c6e13925
Godxin9 <you@example.com>
2021-10-02 14:18:35 +0800

    vue脚手架基本框架--第一次提交

---
+++ src/app.vue
@@ -0,0 +1,12 @@
+<template>
+    <myButton/>
+</template>
+
+<script>
+import myButton from '@/components/myButton.vue'
+export default {
+    components:{
+        myButton
+    }
+}
+</script>
---
+++ src/components/myButton.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div>
+        <h1>这是一个myButton组件</h1>
+        <button @click="msg++">点击次数：{{msg}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg:0,
+        }
+    },
+    methods:{
+        click(){
+            this.msg+=1
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: hotpink;
+    }
+</style>
